369dda963e7379245060361e5644da6b0f22dbc9
valentin_verin <vverinpro@gmail.com>
2025-08-20 11:50:26 +0200

    analitycs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
